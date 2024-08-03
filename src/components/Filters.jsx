409073f605660e2c0a844803a0303b5a6ec87ceb
mohammad shoaib <mohammadshoaib7221@gmail.com>
2024-08-03 15:31:49 +0530

    fix search bar

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 const Filters = ({ searchTerm, setSearchTerm }) => {
   return (
     <div className="flex flex-col p-4 bg-gray-100 dark:bg-gray-900">
@@ -10,7 +9,7 @@ const Filters = ({ searchTerm, setSearchTerm }) => {
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
           placeholder="Search by Title"
-          className="w-full sm:w-1/4 p-2 border border-gray-300 dark:border-gray-700 rounded bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 placeholder-gray-400 dark:placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-400 dark:focus:ring-gray-600"
+          className="w-full p-2 border border-gray-300 dark:border-gray-700 rounded bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 placeholder-gray-400 dark:placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-400 dark:focus:ring-gray-600"
         />
       </div>
     </div>
